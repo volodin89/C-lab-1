9384ef605cedc7caf7b24925e09e0c317ab54265
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2018-10-29 16:36:22 +0300

    Update test_lab1.cpp

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -7,7 +7,7 @@
 
 TEST(task1, check_weight_m)
 {
-	ASSERT_EQ(0,getRecommendation('m',170,70));
+    ASSERT_EQ(0,getRecommendation('m',170,70));
     ASSERT_EQ(1,getRecommendation('m',160,70));
     ASSERT_EQ(-1,getRecommendation('m',190,70));
 }
@@ -15,21 +15,21 @@ TEST(task1, check_weight_m)
 
 TEST(task1, check_weight_w)
 {
-	ASSERT_EQ(0,getRecommendation('w',170,60));
+    ASSERT_EQ(0,getRecommendation('w',170,60));
     ASSERT_EQ(1,getRecommendation('w',160,70));
     ASSERT_EQ(-1,getRecommendation('w',180,50));	
 }
 
 
-TEST(task2, check_greet_right)
+TEST(task2, check_greet_uncorrect)
 {
-	ASSERT_STREQ("Uncorrect time!",greet(25,30));
+    ASSERT_STREQ("Uncorrect time!",greet(25,30));
 	
 }
 
-TEST(task2, check_greet_uncorr)
+TEST(task2, check_greet_right)
 {
-	ASSERT_STREQ("Good day!",greet(12,30));
-	ASSERT_STREQ("Good night!",greet(5,10));
+    ASSERT_STREQ("Good day!",greet(12,30));
+    ASSERT_STREQ("Good night!",greet(5,10));
     ASSERT_STREQ("Good evening!",greet(21,04));
 }
