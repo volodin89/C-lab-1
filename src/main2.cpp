ca7449d135edb0eb4b1e9b111849f1a684b5e706
volodin89 <44975344+volodin89@users.noreply.github.com>
2018-12-13 12:01:51 +0300

    Update main2

--- src/main2.cpp
+++ src/main2.cpp
@@ -6,8 +6,8 @@ int main()
 	int hour = 0;
 	int min = 0;
 
-	printf("Enter the time (hh,mm,ss): ");
-	scanf("%d,%d", &hour, &min);
+	printf("Enter the time (hh:mm:ss): ");
+	scanf("%d:%d", &hour, &min);
 
 	printf("%s", greet(hour, min));
 	return 0;
